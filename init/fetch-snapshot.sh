1aa44d192b1c71b07ec9322f93a457c1783dedce
George Gatheca <george.gatheca@galaxy.com>
2026-06-03 18:29:39 +0100

    make fetch-snapshot executable

--- init/fetch-snapshot.sh
+++ init/fetch-snapshot.sh
@@ -19,7 +19,7 @@ if [[ ! -f /data/.initialized ]]; then
     cd /data/snapshot
     aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true -o snapshot.tar.gz "${SNAPSHOT}"
 
-    # Extract
+    # Extract 
     tar -zxvf snapshot.tar.gz
     mv /data/data/public /data/data/public_bak
     mv public/ /data/data
